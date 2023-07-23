b5e303e4fcf7200305db34f541d880c0e732d77e
fourhu49 <125254585+fourhu49@users.noreply.github.com>
2023-07-23 10:25:03 +0800

    merge sort

--- sort.py
+++ sort.py
@@ -32,4 +32,25 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("排序后的数组:") 
 for i in range(n): 
-    print ("%d" %arr[i]),
+    print ("%d" %arr[i])
+
+def mergeSort(arr):
+    import math
+    if(len(arr)<2):
+        return arr
+    middle = math.floor(len(arr)/2)
+    left, right = arr[0:middle], arr[middle:]
+    return merge(mergeSort(left), mergeSort(right))
+
+def merge(left,right):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0));
+    while left:
+        result.append(left.pop(0))
+    while right:
+        result.append(right.pop(0));
+    return result
